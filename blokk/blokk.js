32343349909cdb4a626eae798f460b659e4ee4f3
Vasilis van Gemert <vvangemert@raapstelen.local>
2013-11-08 14:37:59 +0100

    Added a function to display text as blocks
    Meant to get a more abstract view of proportions. Not necessary, but
    can be helpful

---
+++ blokk/blokk.js
@@ -0,0 +1,32 @@
+var blokkButton = '<button id="blokk-button">blokk</button>';
+document.body.innerHTML += blokkButton;
+
+document.getElementById('blokk-button').onclick = function(){
+	blokkAll();
+}
+
+function blokkAll(){
+	if (document.body.className.indexOf('blokk') < 0) {
+		var pargs = document.querySelectorAll('p, h1'),
+			i = 0,
+			j = 0,
+			withSpans = '';
+		console.log(pargs);
+		while (i < pargs.length) {
+			j = 0;
+			pText = pargs[i].textContent;
+			pWords = pText.split(' ');
+			while( j < pWords.length ) {
+				withSpans += '<span>' + pWords[j] + '</span> ';
+				j++;
+			}
+			pargs[i].innerHTML = withSpans;
+			withSpans = '';
+			document.body.className += (' blokk');
+			i++;
+		}
+	}
+	else {
+		document.body.className = '';
+	}
+}
