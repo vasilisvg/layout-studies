969a7d1129255b77a8d46aab33b454c6ce06776b
Vasilis van Gemert <vvangemert@raapstelen.fritz.box>
2013-11-16 09:40:15 +0100

    Started with adding image layouts
    Created a first image item, added some images, and created a first
    image tool.

--- blokk/blokk.js
+++ blokk/blokk.js
@@ -1,5 +1,5 @@
 var blokkButton = '<button id="blokk-button">blokk</button> <button id="border-button">border</button>';
-document.body.innerHTML += blokkButton;
+document.getElementById('tools').innerHTML += blokkButton;
 
 document.getElementById('blokk-button').onclick = function(){
 	blokkAll();
@@ -7,7 +7,16 @@ document.getElementById('blokk-button').onclick = function(){
 document.getElementById('border-button').onclick = function(){
 	borderAll();
 }
-
+document.querySelector("[href='#explanation']").onclick = function() {
+	document.getElementById('explanation').className = 'is-active';
+	document.getElementById('explanation').focus();
+	return false;
+}
+document.querySelector("[href='#top']").onclick = function() {
+	document.getElementById('explanation').className = '';
+	document.querySelector("[href='#explanation']").focus();
+	return false;
+}
 function blokkAll(){
 	if (document.body.className.indexOf('blokk') < 0) {
 		var pargs = document.querySelectorAll('.marginal-note, p, h1, h2'),
@@ -40,5 +49,14 @@ function borderAll() {
 	else {
 		var olddiv = document.getElementById('borderCSS');
 		document.querySelector('head').removeChild(olddiv);
+	}
+}
+
+function showCaption(){
+	if ( document.querySelector('.explanation') !== null) {
+		document.querySelector('.explanation').className = 'has-explanation';
+	}
+	else {
+		document.querySelector('.has-explanation').className = 'explanation';
 	}
 }
